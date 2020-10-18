00dbceeffa314004aa879b530350c62f37c755ac
Gergely Csatari <gergely.csatari@gmail.com>
2020-10-18 17:08:22 +0200

    negyedik

---
+++ hatodikhet/hatodikhet/Entites/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hatodikhet.Entites
+{
+    class RateData
+    {
+        DateTime Date;
+        string Currency;
+        decimal Value;
+    }
+}
--- hatodikhet/hatodikhet/Form1.cs
+++ hatodikhet/hatodikhet/Form1.cs
@@ -1,4 +1,5 @@
-﻿using hatodikhet.MnbServiceReference;
+﻿using hatodikhet.Entites;
+using hatodikhet.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,12 @@ namespace hatodikhet
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            dataGridView1.DataSource = Rates;
         }
 
         private void GetExchangeRates()
