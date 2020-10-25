1e8f8485b1eb5c0fb075b5c9d829bb3ad2a84379
Gergely Csatari <gergely.csatari@gmail.com>
2020-10-25 23:20:20 +0100

    vege

--- hetedikhet/hetedikhet/Form1.Designer.cs
+++ hetedikhet/hetedikhet/Form1.Designer.cs
@@ -60,7 +60,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(299, 15);
+            this.textBox1.Location = new System.Drawing.Point(299, 16);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(327, 20);
             this.textBox1.TabIndex = 2;
